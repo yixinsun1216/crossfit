503202ef0b8b7bbb3c6b511c657c54ed84ac1c86
Yixin Sun <yixinsun1216@gmail.com>
2019-08-15 10:42:53 -0500

    change name from dml to crossfit

---
+++ R/ml_functions.R
@@ -0,0 +1,59 @@
+#' Machine Learning Functions
+#'
+#' \code{regression_forest2} estimates a regression forest with a formula
+#'   instead of manually passing matrices.
+#' \code{predict_rf2} predicts based on the forest ouputted from
+#'   \code{regression_forest2}.
+
+#' @param f an object of class formula representing the model to be fitted.
+#' @param d a dataframe containing the variables in f.
+#' @param ... additional arguments taht can be passed to
+#'   \code{\link[grf:regression_forest]{regression_forest}},such as num.trees,
+#'   honesty,honesty.fraction,seed.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom tibble tibble as_tibble enframe
+#' @importFrom purrr pluck
+#' @importFrom stats update formula model.matrix model.frame predict
+#' @importFrom grf regression_forest
+#' @importFrom Formula Formula
+#'
+#' @export
+regression_forest2 <- function(f, d, ...) {
+  f <- Formula(f)
+
+  Y <-
+    f %>%
+    formula(lhs = 1, rhs = 0) %>%
+    model.frame(d) %>%
+    as.matrix
+
+  X <-
+    formula(f, rhs = 1, lhs = 0) %>%
+    update(~ 0 + .) %>%
+    model.matrix(d)
+
+  ff <- regression_forest(X, Y, ...)
+
+  ff[["formula"]] <- f
+  class(ff) <- c("regression_forest", "grf")
+
+  return(ff)
+}
+
+#' @export
+#' @rdname regression_forest2
+predict_rf2 <- function(forest, newdata = NULL) {
+  f <- forest[["formula"]]
+
+  if(!is.null(newdata)) {
+    X <-
+      formula(f, rhs = 1, lhs = 0) %>%
+      update(~ 0 + .) %>%
+      model.matrix(newdata)
+
+    return(pluck(predict(forest, X), "predictions"))
+  } else {
+    return(pluck(predict(forest), "predictions"))
+  }
+}
