02bf377bc25bc9ae371eca8fb654c9ffba155da4
Yixin Sun <yixinsun1216@gmail.com>
2019-09-13 09:25:23 -0500

    creating overarching machine learning functions that calls the user specified ml for estimation

--- R/dml.R
+++ R/dml.R
@@ -58,109 +58,11 @@ dml <- function(f, d, psi, psi_grad, psi_op, n = 101, nw = 4,
   dml_seed <- if_else(is.null(dml_seed), FALSE, dml_seed)
 
   seq(1, nn) %>%
-    future_map(~dml_step(f, d, psi, psi_grad, psi_op, ml),
+    future_map(~dml_step(f, d, psi, psi_grad, psi_op, dml_seed, ml),
                .options = future_options(packages = c("splines"),
                                          seed = dml_seed)) %>%
     get_medians(nrow(d), dml_call)
 }
-
-#' @export
-#' @rdname dml
-### try implementing a "fast" dml which just uses a single forest
-dml_fast <- function(f, d, psi, psi_grad, psi_op, dml_seed = NULL) {
-  # step 0: ensure fm has no intercept in the parametric part
-  f <-
-    Formula(f) %>%
-    update(. ~ 0 + . | 0 + .)
-
-  # step 1: make the estimation dataset
-  # (a) expand out any non-linear formula for y and sanitize names
-  ty <- get_lhs_col(f, d)
-
-  # (b) expand out any non-linear formula for d and sanitize names
-  td <- get_rhs_cols(f, d, 1)
-
-  # (c) expand out any non-linear formula for x and sanitize names
-  # NOTE: no real reason to have nonlinear formulae here but might as well
-  # be robust to it
-  tx <- get_rhs_cols(f, d, 2)
-
-  # (c) make a new dataset of transformed y, transformed d and (transformed) x
-  newdata <- bind_cols(ty, td, tx)
-
-  # step 2: save formulae for gamma and delta, based on transformed y and d
-  xvars <- paste("0", paste0(names(tx), collapse = " + "), sep = " + ")
-
-  f_gamma <-
-    paste(names(ty), xvars, sep = " ~ ") %>%
-    as.formula
-
-  fs_delta <-
-    names(td) %>%
-    map(~ paste(., xvars, sep = " ~ ")) %>%
-    map(as.formula)
-
-  # step 3: train models for delta and gamma
-  gamma_model <- regression_forest2(f_gamma, newdata,
-                                    num.trees = 10000,
-                                    honesty = TRUE,
-                                    honesty.fraction = NULL,
-                                    seed = dml_seed)
-
-  delta_models <-
-    fs_delta %>%
-    map(~ regression_forest2(., newdata,
-                             num.trees = 10000,
-                             honesty = TRUE,
-                             honesty.fraction = NULL,
-                             seed = dml_seed))
-
-  # step 4: estimate OOB values of delta and gamma
-  gamma <-
-    predict_rf2(gamma_model) %>%
-    as.matrix
-
-  delta <-
-    delta_models %>%
-    map(predict_rf2) %>%
-    unlist %>%
-    matrix(ncol = length(fs_delta))
-
-  # step 5: generate Y and D with useful names
-  ynames <- names(ty)
-  Y <- as.matrix(select(as.data.frame(newdata), !!ynames))
-
-  dnames <- names(td)
-  D <- as.matrix(select(as.data.frame(newdata), !!dnames))
-
-  # step 6: do a single DML step
-  obj <- function(theta) psi(theta, Y, D, gamma, delta)
-
-  grad <- function(theta) psi_grad(theta, Y, D, gamma, delta)
-
-  theta0 <- rep(0, dim(D)[2])
-  names(theta0) <- colnames(D)
-  theta <-
-    optim(theta0,
-          function(x) square(obj(x)),
-          function(x) 2 * grad(x) %*% obj(x),
-          method = "BFGS",
-          control = list(maxit = 500))
-
-  J0 <- psi_grad(theta$par, Y, D, gamma, delta)
-
-  s2 <- psi_op(theta$par, Y, D, gamma, delta)
-
-  s2 <- solve(t(J0)) %*% s2 %*% solve(J0)
-
-  n <- nrow(newdata)
-  return(structure(list(coefficients = theta$par,
-                        vcov = (1 / n) * s2,
-                        nobs = n),
-                   class = "dml"))
-
-}
-
 
 square <- function(x) 0.5 * t(x) %*% x
 
@@ -231,7 +133,7 @@ get_lhs_col <- function(f, d) {
 
 # estimate theta and s2 in a single sample split of the data
 # formula should be y ~ d | x
-dml_step <- function(f, d, psi, psi_grad, psi_op, dml_seed = NULL, ...) {
+dml_step <- function(f, d, psi, psi_grad, psi_op, dml_seed = NULL, ml, ...) {
   # step 0: ensure fm has no intercept in the parametric part
   f <-
     Formula(f) %>%
@@ -267,36 +169,12 @@ dml_step <- function(f, d, psi, psi_grad, psi_op, dml_seed = NULL, ...) {
     map(~ paste(., xvars, sep = " ~ ")) %>%
     map(as.formula)
 
+  # Next 2 steps done in the function run_ml()
   # step 3: train models for delta and gamma
-  gamma_models <- map(folds$test, function(y) {
-    regression_forest2(f_gamma, d = y,
-                       num.trees = 1000,
-                       honesty = FALSE,
-                       honesty.fraction = NULL,
-                       seed = dml_seed)
-  })
-
-  delta_models <-
-    folds$test %>%
-    map(function(x) map(fs_delta, function(y) {
-      regression_forest2(y, d = x,
-                         num.trees = 1000,
-                         honesty = FALSE,
-                         honesty.fraction = NULL,
-                         seed = dml_seed)
-    }))
-
   # step 4: estimate values of delta and gamma in the hold out sample
-  gamma <-
-    map2(gamma_models, folds$test, ~ predict_rf2(.x, .y)) %>%
-    map(as.matrix)
+  gamma <- run_ml(f_gamma, folds$test, ml, "gamma", dml_seed, ...)
+  delta <- run_ml(fs_delta, folds$test, ml, "delta", dml_seed, ...)
 
-  delta <-
-    map2(delta_models, folds$test,
-         function(x, y)
-           map2(x, list(y), ~ predict_rf2(.x, .y)) %>%
-           unlist %>%
-           matrix(ncol = length(fs_delta)))
 
   # step 5: put together the values of Y and D in the hold out sample, and pass
   # things off to the dml_estimate routine
--- R/ml_functions.R
+++ R/ml_functions.R
@@ -20,13 +20,10 @@
 #'
 #'
 
-default_forest <- list(num.trees = 1000,
-                       honesty = TRUE,
-                       honesty.fraction = NULL)
-
 #' @export
+#' note on passing in arguments https://stackoverflow.com/questions/23922130/default-argument-in-r-function-formal-argument-matched-by-multiple-actual-argum?rq=1
 regression_forest2 <- function(f, d, num.trees = 1000, honesty = TRUE,
-                               honesty.fraction = NULL) {
+                               honesty.fraction = NULL, ...) {
   f <- Formula(f)
 
   Y <-
@@ -40,7 +37,8 @@ regression_forest2 <- function(f, d, num.trees = 1000, honesty = TRUE,
     update(~ 0 + .) %>%
     model.matrix(d)
 
-  ff <- regression_forest(X, Y, default_forest)
+  ff <- regression_forest(X, Y, num.trees = num.trees, honesty = honesty,
+                          honesty.fraction = honesty.fraction, ...)
 
   ff[["formula"]] <- f
   class(ff) <- c("regression_forest", "grf")
@@ -58,7 +56,6 @@ predict_rf2 <- function(forest, newdata = NULL) {
       formula(f, rhs = 1, lhs = 0) %>%
       update(~ 0 + .) %>%
       model.matrix(newdata)
-
     return(pluck(predict(forest, X), "predictions"))
   } else {
     return(pluck(predict(forest), "predictions"))
@@ -69,4 +66,46 @@ cv.glmnet2 <- function(f, d, ...){
 
   lf <- cv.glmnet(f, data = d)
 
+}
+
+
+#' @export
+run_ml <- function(f, folds_test, ml_type, value_type,
+                   dml_seed = NULL, ...){
+  # specify which ml method to use for training and predicting
+  if(ml_type == "regression_forest"){
+    train_ml <- "regression_forest2"
+    predict_ml <- "predict_rf2"
+  }
+
+  if(value_type == "gamma"){
+    print("made it")
+    gamma_models <- map(folds_test, function(y) {
+      get(train_ml)(f, d = y, seed = dml_seed, ...)
+    })
+
+    output <-
+      map2(gamma_models, folds_test, ~ get(predict_ml)(.x, .y)) %>%
+      map(as.matrix)
+
+  }
+
+  if(value_type == "delta"){
+    # step 3: train models for delta and gamma
+    delta_models <-
+      folds_test %>%
+      map(function(x) map(f, function(y) {
+        get(train_ml)(y, d = x, seed = dml_seed, ...)
+      }))
+
+    # step 4: estimate values of delta and gamma in the hold out sample
+    output <-
+      map2(delta_models, folds_test,
+           function(x, y)
+             map2(x, list(y), ~ get(predict_ml)(.x, .y)) %>%
+             unlist %>%
+             matrix(ncol = length(fs_delta)))
+  }
+
+  return(output)
 }
