9f110d8417850f652a54070cda38ad29e6310ceb
Yixin Sun <yixinsun1216@gmail.com>
2020-03-30 17:24:25 -0500

    first pass at estimating theta using original dml formulation

--- R/dml_original.R
+++ R/dml_original.R
@@ -1,5 +1,11 @@
 dml_step_original <- function(f, d, model, dml_seed = NULL,
                               ml, poly_degree, ...){
+  if(model == "poisson"){
+    psi <<- psi_plpr
+    psi_grad <<- psi_plpr_grad
+    psi_op <<- psi_plpr_op
+  }
+
    # ensure fm has no intercept in the parametric part
   f <-
     Formula(f) %>%
@@ -8,14 +14,17 @@ dml_step_original <- function(f, d, model, dml_seed = NULL,
   # step 1: make the estimation dataset
   # (a) expand out any non-linear formula for y and sanitize names
   ty <- get_lhs_col(f, d)
+  ynames <- names(ty)
 
   # (b) expand out any non-linear formula for d and sanitize names
   td <- get_rhs_cols(f, d, 1)
+  dnames <- names(td)
 
   # (c) expand out any non-linear formula for x and sanitize names
   # NOTE: no real reason to have nonlinear formulae here but might as well
   # be robust to it
   tx <- get_rhs_cols(f, d, 2)
+  xnames <- names(tx)
 
   # (c) make a new dataset of transformed y, transformed d and (transformed) x
   newdata <- bind_cols(ty, td, tx)
@@ -27,8 +36,8 @@ dml_step_original <- function(f, d, model, dml_seed = NULL,
   # 1. Calculate theta and beta using training data
   # 2. Use training data and estimates from 1 to find mu
   # 3. Optimize over psi using hold out data
-  f_ml <- paste(c(names(tx), names(td)), collapse = " + ") %>%
-    paste0(names(ty), " ~ ", .) %>%
+  f_ml <- paste(c(xnames, dnames), collapse = " + ") %>%
+    paste0(ynames, " ~ ", .) %>%
     as.formula
 
   # Step 1:
@@ -38,9 +47,45 @@ dml_step_original <- function(f, d, model, dml_seed = NULL,
   # mu = E[X'exp(D*theta + X*beta)X]^{-1}E[X'exp(D*theta - X*beta)D]
   mu_hat <-
     map2(ml_coefs, folds$train,
-          function(x, y) estimate_mu(x, y, names(td), names(tx)))
+          function(x, y) estimate_mu(x, y, dnames, xnames))
+
+  beta_hat <-
+    map(ml_coefs, function(x) {
+      filter(x, coef %in% xnames) %>%
+        arrange(order(match(coef, xnames))) %>%
+        pull(value)})
 
   # Step 3:
+  Y <- map(folds$test, ~ as.matrix(select(as.data.frame(.), !!ynames)))
+  D <- map(folds$test, ~ as.matrix(select(as.data.frame(.), !!dnames)))
+  X <- map(folds$test, ~ as.matrix(select(as.data.frame(.), !!xnames)))
+
+  obj <- function(theta) {
+    list(Y, D, X, mu_hat, beta_hat) %>%
+      pmap(function(Y, D, X, mu_hat, beta_hat) {
+        psi(theta, Y, D, X, mu_hat, beta_hat)
+      }) %>%
+      reduce(`+`) / length(D)
+  }
+
+  grad <- function(theta) {
+    list(D, X, mu_hat, beta_hat) %>%
+      pmap(function(D, X, mu_hat, beta_hat) {
+        psi_grad(theta, D, X, mu_hat, beta_hat)
+      }) %>%
+      reduce(`+`) / length(D)
+  }
+
+  theta0 <- rep(0, dim(D[[1]])[2])
+  names(theta0) <- colnames(D[[1]])
+  theta <-
+    optim(theta0,
+          function(x) square(obj(x)),
+          function(x) 2 * grad(x) %*% obj(x),
+          method = "BFGS",
+          control = list(maxit = 500))
+
+  return(theta)
 
 }
 
@@ -112,11 +157,42 @@ estimate_mu <- function(coefs, train_fold, d_names, x_names){
       j_calc(x, y, d_coefs$value, x_coefs$value, intercept, "bb")) %>%
     reduce(`+`)/length(x_list)
 
-  return(solve(j_bb) %*% j_tb)
+  return(j_tb %*% solve(j_bb))
 }
 
 j_calc <- function(X, D, theta, beta, int, type){
   V <- D
-  if(type != "tb") V <- X
-  t(as.matrix(X*exp(sum(D[1,]*theta) + sum(X*beta) + int))) %*% as.matrix(V)
+  if(type == "bb") V <- X
+  t(as.matrix(V*exp(sum(D*theta) + sum(X*beta) + int))) %*% as.matrix(X)
+}
+
+# ============================================================================
+# moment functions for poisson
+# ============================================================================
+psi_plpr <- function(theta, Y, D, X, mu, beta){
+  theta <- matrix(theta, nrow = dim(D)[2])
+  beta <- matrix(beta, nrow = dim(X)[2])
+  N <- nrow(Y)
+  return((1/N) * t(D - X %*% t(mu)) %*% (Y - exp(D %*% theta + X %*% beta)))
+}
+
+# psi_plpr <- function(theta, Y, D, X, mu, beta){
+#   y_list <- split(Y, seq(nrow(Y)))
+#   x_list <- split(X, seq(nrow(X)))
+#   d_list <- split(D, seq(nrow(D)))
+#
+#   list(y_list, x_list, d_list) %>%
+#     pmap(function(y, x, d) {
+#     as.integer(y - exp(d %*% theta + x %*% beta))*t(d - x %*% t(mu))}) %>%
+#     reduce(`+`)/nrow(Y)
+# }
+
+
+psi_plpr_grad <- function(theta, D, X, mu, beta){
+  x_list <- split(X, seq(nrow(X)))
+  d_list <- split(D, seq(nrow(D)))
+
+  map2(x_list, d_list, function(x, d) {
+      (-t(d - x %*% t(mu))) %*% exp(d %*% theta + x %*% beta)%*% d}) %>%
+    reduce(`+`)/nrow(X)
 }
