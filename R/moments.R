2434d4e500157406a16b064bfd965b6db40d7289
yixinsun1216 <yixinsun1216@gmail.com>
2020-09-05 15:23:21 -0400

    Issue7 implement poisson (#9)
    * change model.frame
    
    * small changes
    
    * first pass at calculating mu_hat in original dml formulation
    
    * first pass at estimating theta using original dml formulation
    
    * really sparse version of new dml function is up and running
    
    * #7 reformulate how to run machine learning estimates
    
    * Update dml_original.R
    
    * #7 basic writeup of how I thikn we should do Poisson lasso
    
    * #7 more clarification
    
    * #7 in progress thinking through the covariance estimates
    
    * #7 reminder for me about how the covariance calculation works
    
    * #4 Update poisson dml method following stata methodology
    
    * #7 spit out s instead of beta_hat from estimate_weights
    
    * #7 revamp dml_original to linear model with a new dml_fold function that fills in moment conditions within each fold
    
    * #7 first pass at trying to fix gradient bug
    
    * #7 implemented linear lasso using non-concentrating out approach - now onto poisson
    
    * #7 finish implementing linear and poisson dml_original using cv.glmnet
    
    * #7 fix issue with ml = rlasso
    
    * #7 round 1 of implementing random forest using Z'(Y - s - D*theta)
    
    * #7 testing out concentrating out vs finite nuisance parameter approach
    
    * #7 merge dml and dml_original aka finite nuisance parameter approach and concentrating out approach
    
    * #7 add parallelising back in
    
    * #7 add documentation for new arguments
    
    * #7 add warning if poisson is used with rlasso
    
    * #7 merge in dml_concentrate into main dml function
    
    * #7 get rid of poisson lasso in concentrating  out approach
    
    * #7 document all functions for different dml methods
    
    * #7 small change in notes
    
    * #7 get rid of rlasso, finite rf, and formula cv.glmnet
    
    * #7 updated errors in dml note
    
    * #7 small change
    
    * #7 adding ml back into dml_fold
    
    * #7 speed up cv.glmnet by doing one big cv.glmnet before dml_fold
    
    * #7 fixed bug in poisson - drop penalty.factor
    
    * #7 get rid of unnecessary print statement
    
    * #7 add in ellipses to pass in more arguments
    
    * #7 allowing for user to choose lambda
    
    Co-authored-by: Thomas Covert <thomc@alum.mit.edu>

--- R/moments.R
+++ R/moments.R
@@ -5,40 +5,92 @@
 #' @param theta scalar or vector of the treatment effect parameter(s).
 #' @param Y a vector of the outcome variable.
 #' @param D a vector or data.frame of the treatment variable(s) of interest.
-#' @param gamma a vector of E[Y|X=x], where X is a vector of controls.
-#' @param delta a vector or data.frame of E[D|X=x].
-#'
-#'
+#' @param s a vector of E[Y|X=x], where X is a vector of controls.
+#' @param Z a vector or data.frame of D - E[D|X=x].
+
+
+# ============================================================================
+# moment functions for linear model
+# ============================================================================
+# finite nuisance parameter approach
 #' @export
-# partial linear model
-# recall this is (Y - gamma(X) - theta * (D - delta(X))) * (D - delta(X))
-psi_plr <- function(theta, Y, D, gamma, delta) {
-  theta <- matrix(theta, nrow = dim(D)[2])
-  N <- nrow(Y)
-  return((1 / N) * t(D - delta) %*% (Y - gamma - (D - delta) %*% theta))
+psi_plr <- function(theta, Y, D, m, s){
+  return(1/nrow(D) * t(D - m) %*% (Y - s - D  %*% theta))
 }
 
 #' @export
-psi_plr_grad <- function(theta, Y, D, gamma, delta) {
-  N <- nrow(Y)
-  return(-1 * (1 / N) * t(D - delta) %*% (D - delta))
+psi_plr_grad <- function(theta, Y, D, m, s){
+  return(-1/nrow(D) * t(D- m) %*% D)
 }
 
 #' @export
-psi_plr_op <- function(theta, Y, D, gamma, delta) {
+psi_plr_op <- function(theta, Y, D, m, s) {
   theta <- matrix(theta, nrow = dim(D)[2])
-  N <- nrow(Y)
-  op <- (D - delta) * as.vector(Y - gamma - (D - delta) %*% theta)
+  N <- nrow(D)
+  op <- (D - m) * as.vector(Y - s - D %*% theta)
   return((1 / N) * t(op) %*% op)
 }
 
+# concentrating out approach -------------------------------------
+#' @export
+psi_plr_conc <- function(theta, Y, D, m, s){
+  return(1/nrow(D) * t(D - m) %*% (Y - s - (D - m) %*% theta))
+}
+
+#' @export
+psi_plr_grad_conc <- function(theta, Y, D, m, s){
+  return(-1/nrow(D) * t(D - m) %*% (D - m))
+}
+
+#' @export
+psi_plr_op_conc <- function(theta, Y, D, m, s) {
+  theta <- matrix(theta, nrow = dim(D)[2])
+  N <- nrow(D)
+  op <- (D - m) * as.vector(Y - s - (D - m) %*% theta)
+  return((1 / N) * t(op) %*% op)
+}
+
+# ============================================================================
+# moment functions for poisson
+# ============================================================================
+#' @export
+psi_plpr <- function(theta, Y, D, m, s){
+  theta <- matrix(theta, nrow = dim(D)[2])
+
+  N <- nrow(D)
+  return((1/N) * t(D - m) %*% (Y - exp(D %*% theta + s)))
+}
+
+#' @export
+psi_plpr_grad <- function(theta, Y, D, m, s){
+  s_list <- split(s, seq(nrow(s)))
+  d_list <- split(D, seq(nrow(D)))
+  N <- nrow(D)
+
+  row_mult <- map2_dfr(d_list, s_list, function(d, s1) as.matrix(d) %*% exp(d %*% theta + s1))
+  return(-1/N * t(D - m) %*% t(row_mult))
+}
+
+#' @export
+psi_plpr_op <- function(theta, Y, D, m, s){
+  theta <- matrix(theta, nrow = dim(D)[2])
+  Z <- D - m
+  z_list <- split(Z, seq(nrow(Z)))
+  op_list <- split((Y - exp(D %*% theta + s)), seq(nrow(Y)))
+
+  N <- nrow(D)
+  op <- as.matrix(map2_dfr(z_list, op_list, function(x, y) x*y))
+  return((1 / N) *  op %*% t(op))
+}
+
+# Concentrating out approach for poisson -------------------------------------
 #' @export
 # partially linear poisson model
 # recall this is:
-# (Y - exp(D*theta) * gamma(X) * z(theta, X)) *
-# (D - exp(theta) * delta(X) * z(theta, X))
-# where z(theta, X) = 1 / (exp(theta) * delta(X) + 1 - delta(X))
-psi_plpr_with_grad <- function(theta, Y, D, gamma, delta) {
+# (Y - exp(D*theta) * s(X) * z(theta, X)) *
+# (D - exp(theta) * m(X) * z(theta, X))
+# where z(theta, X) = 1 / (exp(theta) * m(X) + 1 - m(X))
+psi_plpr_with_grad <- function(theta, Y, D, m, s) {
   # first verify that D is univariate and 0/1 valued
   K <- ncol(D)
   nvals <- length(unique(D))
@@ -48,32 +100,32 @@ psi_plpr_with_grad <- function(theta, Y, D, gamma, delta) {
   if(!(K == 1 & nvals == 2 & minval == 0 & maxval == 1)) {
     stop("treatment variable not univariate and binary")
   } else {
-    z <- 1 / (exp(theta) * delta + 1 - delta)
-    A <- (Y - exp(D * theta) * gamma * z)
-    B <- (D - exp(theta) * delta * z)
+    z <- 1 / (exp(theta) * m + 1 - m)
+    A <- (Y - exp(D * theta) * s * z)
+    B <- (D - exp(theta) * m * z)
 
     # psi is A * B so gradient is dA * B + A * dB
-    dz <- -1 * (exp(theta) * delta) * z^2
-    dA <- -1 * (D * exp(D * theta) * gamma * z + exp(D * theta) * gamma * dz)
-    dB <- -1 * (exp(theta) * delta * z + exp(theta) * delta * dz)
+    dz <- -1 * (exp(theta) * m) * z^2
+    dA <- -1 * (D * exp(D * theta) * s * z + exp(D * theta) * s * dz)
+    dB <- -1 * (exp(theta) * m * z + exp(theta) * m * dz)
 
     return(list(A * B, dA * B + A * dB))
   }
 }
 
 #' @export
-psi_plpr <- function(theta, Y, D, gamma, delta) {
-  return(mean(psi_plpr_with_grad(theta, Y, D, gamma, delta)[[1]]))
+psi_plpr_conc <- function(theta, Y, D, m, s) {
+  return(mean(psi_plpr_with_grad(theta, Y, D, m, s)[[1]]))
 }
 
 #' @export
-psi_plpr_grad <- function(theta, Y, D, gamma, delta) {
-  return(mean(psi_plpr_with_grad(theta, Y, D, gamma, delta)[[2]]))
+psi_plpr_grad_conc <- function(theta, Y, D, m, s) {
+  return(mean(psi_plpr_with_grad(theta, Y, D, m, s)[[2]]))
 }
 
 #' @export
-psi_plpr_op <- function(theta, Y, D, gamma, delta) {
-  vals <- psi_plpr_with_grad(theta, Y, D, gamma, delta)[[1]]
+psi_plpr_op_conc <- function(theta, Y, D, m, s) {
+  vals <- psi_plpr_with_grad(theta, Y, D, m, s)[[1]]
   return(mean(vals^2))
 }
 
